3129d099631b4bfce18fcfdf9665977b98151236
DanielJames830 <danielmanley830@gmail.com>
2024-03-26 11:47:38 -0400

    created user endpoint

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const userRouter = require('./routers/user');
 
 // Configure express to allow requests from anywhere
-app.use(cors())
+app.use(cors());
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -13,18 +14,22 @@ app.use(function (req, res, next) {
 // Configure express to read JSON objects in the body of requests.
 app.use(express.json());
 
+// Define routers here
+app.use(userRouter);
+
 // Set up a default route ('') and return 'Hello World!' in the 
 // response when requests are received
 app.get('', (req, res) => {
-    res.send('Hello World!')
-})
+    res.send('Hello World!');
+});
+
 
 // Set port to the PORT environment variable (if it is defined), 
 // otherwise set it to 3000 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 // Configure the server to listen for connections on the port. 
 // Print to the console when ready for connections
 app.listen(port, () => {
-    console.log('Server is up on port ' + port)
-})
+    console.log('Server is up on port ' + port);
+});
