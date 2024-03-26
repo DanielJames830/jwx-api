ad697018d04a5dd4e7f2f16df89d890ffc0475ce
DanielJames830 <danielmanley830@gmail.com>
2024-03-26 18:07:36 -0400

    Firebase

--- src/app.js
+++ src/app.js
@@ -1,35 +1,57 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const userRouter = require('./routers/user');
+const testRouter = require("./routers/test");
+const {
+	initializeApp,
+	applicationDefault,
+	cert,
+} = require("firebase-admin/app");
+const {
+	getFirestore,
+	Timestamp,
+	FieldValue,
+	Filter,
+} = require("firebase-admin/firestore");
+const {connect} = require('firefose');
 
 // Configure express to allow requests from anywhere
 app.use(cors());
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	next();
 });
 
 // Configure express to read JSON objects in the body of requests.
 app.use(express.json());
 
 // Define routers here
-app.use(userRouter);
+app.use(testRouter);
 
-// Set up a default route ('') and return 'Hello World!' in the 
+// Set up a default route ('') and return 'Hello World!' in the
 // response when requests are received
-app.get('', (req, res) => {
-    res.send('Hello World!');
+app.get("", (req, res) => {
+	res.send("Hello World!");
 });
 
+async function initializeFirebase() {
+	const credentials = require("../env/jwx-api-4df70568a1ab.json");
 
-// Set port to the PORT environment variable (if it is defined), 
-// otherwise set it to 3000 
+	await connect(credentials, "databaseURI")
+}
+
+initializeFirebase();
+
+// Set port to the PORT environment variable (if it is defined),
+// otherwise set it to 3000
 const port = process.env.PORT || 3000;
 
-// Configure the server to listen for connections on the port. 
+// Configure the server to listen for connections on the port.
 // Print to the console when ready for connections
 app.listen(port, () => {
-    console.log('Server is up on port ' + port);
+	console.log("Server is up on port " + port);
 });
--- src/routers/user.js
+++
@@ -1,9 +0,0 @@
-const express = require('express') 
-const router = express.Router() 
-
-router.post('/user', (req, res) => { 
-    console.log(req.body) 
-    res.status(201).send(req.body) 
-}) 
-
-module.exports = router
